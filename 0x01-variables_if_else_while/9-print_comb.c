80d0afa474ceebeb1dd7729e0553c0c4cdb391b9
GoodTaicho <team2009good@gmail.com>
2023-03-17 14:12:35 -0700

    work40

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+	int main(void)
+	{
 		int i;
 
 		for (i = 48; i < 58; i++)
@@ -14,10 +14,10 @@ int main(void)
 			putchar(i);
 			if (i != 57)
 			{
-																			putchar(',');
-																			putchar(' ');
-																		}
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		putchar('\n');
-		return (0);
-}
+	return (0);
+	}
