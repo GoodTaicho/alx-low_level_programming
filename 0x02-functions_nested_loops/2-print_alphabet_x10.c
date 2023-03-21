ed4fd4fc2dbc296ec77855d01964149be2c0a2cf
GoodTaicho <team2009good@gmail.com>
2023-03-21 13:06:35 -0700

    work8

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 	int ch, i;
 
 	for (i = 0; i <= 9; i++)
-					{
+	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-																		_putchar(ch);
-																	}
+			_putchar(ch);
+																    }
 		_putchar('\n');
 	}
 }
